febe1ebecf9361a82280e3187557d0616d76d963
Mustafagenccc <mstfgncc@gmail.com>
2025-11-10 15:05:20 +0300

    gül db api last

--- src/test/java/snackattack/runners/Runner.java
+++ src/test/java/snackattack/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"snackattack/stepdefs"},
-        tags = "@OrdersDelete",
+        tags = "@Order_item_additions",
 
         dryRun = false
 )
